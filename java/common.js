432657f26bdbeded6e67cc1efe9ab15c77e9897c
cutesori <sori2230@naver.com>
2025-05-14 17:53:37 +0900

    20250514

--- java/common.js
+++ java/common.js
@@ -60,6 +60,21 @@ queries.forEach((item) => {
 
     data.forEach(book => {
       const temp = `
+
+        <div class="swiper mySwiper">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">Slide 1</div>
+      <div class="swiper-slide">Slide 2</div>
+      <div class="swiper-slide">Slide 3</div>
+      <div class="swiper-slide">Slide 4</div>
+      <div class="swiper-slide">Slide 5</div>
+      <div class="swiper-slide">Slide 6</div>
+      <div class="swiper-slide">Slide 7</div>
+      <div class="swiper-slide">Slide 8</div>
+      <div class="swiper-slide">Slide 9</div>
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
         <div class="book">
           <img src="${book.thumbnail}" alt="${book.title}" />
           <strong>${book.title}</strong>
